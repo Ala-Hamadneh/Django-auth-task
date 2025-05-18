c6a732ec6e849d2055ff0004356bc2a90060b66c
Alaa <alahamadneh35@gmail.com>
2025-05-18 15:07:12 +0300

    created new django app (tasks), for manage tasks. I implemented : 1-create new task 2-list all the tasks 3-get specific task details 4-update specific task details 5-delete task by id

---
+++ tasks/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from users.models import CustomUser
+# Create your models here.
+
+class Tasks(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50,null=False)
+    description = models.TextField(blank=True)
+    is_completed = models.BooleanField(default=False)
+    due_date = models.DateField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return self.title
+    
---
+++ tasks/views.py
@@ -0,0 +1,22 @@
+from rest_framework import generics, permissions
+from .models import Tasks
+from .serializers import TaskSerializer
+
+# Create your views here.
+
+class TaskListCreateView(generics.ListCreateAPIView):
+    serializer_class = TaskSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Tasks.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+class TaskRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = TaskSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Tasks.objects.filter(user=self.request.user)
