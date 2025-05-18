06627bd44fa20b586ae310a485d4c4ffbdea00f1
Alaa <alahamadneh35@gmail.com>
2025-05-18 15:02:10 +0300

    created new django app (users), and implemented the user management system: 1-register new user 2-login 3-logut 4-token refresh

---
+++ users/views.py
@@ -0,0 +1,25 @@
+from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.tokens import RefreshToken
+from .models import CustomUser
+from .serializers import RegisterSerializer
+
+# Create your views here.
+
+class RegisterView(generics.CreateAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = RegisterSerializer
+
+class LogoutView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        try:
+            refresh_token = request.data["refresh"]
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+            return Response({"message": "Logout successful"}, status=200)
+        except Exception as e:
+            return Response({"error": str(e)}, status=400)
